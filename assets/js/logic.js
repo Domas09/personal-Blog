325545d679fc0b5b3f9f194c358ad8caccfd3aea
Expired Milk <domasdar2@gmail.com>
2024-03-06 21:33:45 -0600

    Working Dark and light mode added

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,33 @@
+const buttonDarkLight = document.querySelector(".dark-light-mode");
+const bodyMode = document.querySelector("body");
 
+if(localStorage.getItem('darkMode') === null){
+     localStorage.setItem('darkMode', JSON.stringify(true));
+}
+function switchMode(){
+    modeDark = JSON.parse(localStorage.getItem('darkMode'));
+    if(modeDark === true){
+        bodyMode.classList.remove("dark");
+        buttonDarkLight.textContent="☀️";
+        localStorage.setItem('darkMode', JSON.stringify(false));
+    } else {
+        bodyMode.classList.add("dark");
+        buttonDarkLight.textContent= "🌙";
+        localStorage.setItem('darkMode', JSON.stringify(true));
+    }
+}
 
+function confirmMode(){
+    modeDark = JSON.parse(localStorage.getItem('darkMode'));
+    if(modeDark === true){
+        bodyMode.classList.add("dark");
+        buttonDarkLight.textContent= "🌙";
+    } else {
+        bodyMode.classList.remove("dark");
+        buttonDarkLight.textContent="☀️";
+    }
+}
 
+buttonDarkLight.addEventListener('click', switchMode)
+
+confirmMode();
