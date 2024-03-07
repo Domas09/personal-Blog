e2dfde6aed23f78e241abcb854078425dbc10927
Expired Milk <domasdar2@gmail.com>
2024-03-06 19:20:56 -0600

    Added styling to cards

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -5,14 +5,14 @@ function makeCard(title, name, content){
     const card = document.createElement("section");
     card.classList.add('card');
     cardSection.appendChild(card);
-    makeEl("h1", title, card);
-    makeEl("h2", name, card);
-    makeEl("p", content, card);
+    makeEl("", "h1", title, card);
+    makeEl("Author: ","h2", name, card);
+    makeEl("","p", content, card);
 }
 
-function makeEl(elementType, content, section){
+function makeEl(preText, elementType, content, section){
     const el = document.createElement(elementType);
-    el.textContent = content;
+    el.textContent = preText + content;
     section.appendChild(el);
 }
 
@@ -22,3 +22,6 @@ function makeEl(elementType, content, section){
 // }
 
 makeCard("Im bored", "Domas", "Im working on this project and its taking a bit.");
+makeCard("Im exited", "Domas", "I got something to work for once :).");
+makeCard("Im confused", "Domas", "Now something isnt working :(.");
+
